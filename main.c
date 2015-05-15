79af65c662fa5b8067d3d5957d90f789f193355e
ttakaluo <teemu.takaluoma@gmail.com>
2015-05-15 16:48:58 +0300

    version 2.0

--- main.c
+++ main.c
@@ -1,25 +1,54 @@
 #include <stdio.h>
 
-int add (int x, int y);
-int subtract(int x, int y);
-int domath(int (*mathop) (int, int), int x, int y);
+float add (float x, float y);
+float subtract(float x, float y);
+float multiply(float x, float y);
+//float divide(float x, float y);
+float domath(float (*mathop) (float, float), float x, float y);
 
-int add(int x, int y){
+float add(float x, float y){
 	return x + y;
 }
 
-int subtract(int x, int y){
+float subtract(float x, float y){
 	return x - y;
 }
 
-int domath(int (*mathop) (int, int), int x, int y){
+float multiply(float x, float y){
+	return x * y;
+}
+
+/*float divide(float x, float y){
+	return divide x / y;
+}*/
+
+float domath(float (*mathop) (float, float), float x, float y){
 	return (*mathop) (x, y);
 }
 
-int main(){
-	int a = domath(add, 10, 2);
-	printf("10 + 2 gives: %d\n", a);
-	int b = domath(subtract, 10, 2);
-	printf("10 - 2 gives: %d\n", b);
+void getOp(char *op){
+}
+
+void getNum(float *a, float *b){
+	printf("Input number a: ");
+	scanf("%f", a);
+	printf("\nInput number b: ");
+	scanf("%f", b);
+	printf("\n");
+}
+
+int main(void){
+
+	float a,b;
+	char op;
+	getNum(&a, &b);
+	getOp(&op);
+	printf("%f\n", a);
+	printf("%f\n", b);
+
 	return 0;
+////////float a = domath(add, 10, 2);
+////////printf("10 + 2 gives: %d\n", a);
+////////float b = domath(subtract, 10, 2);
+////////printf("10 - 2 gives: %d\n", b);
 }
