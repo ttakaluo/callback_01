739572a7b44fc92f78becc29b1595d804c8d968c
ttakaluo <teemu.takaluoma@gmail.com>
2015-05-15 17:42:12 +0300

    version 3.0

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 float add (float x, float y);
 float subtract(float x, float y);
 float multiply(float x, float y);
-//float divide(float x, float y);
+float divide(float x, float y);
 float domath(float (*mathop) (float, float), float x, float y);
 
 float add(float x, float y){
@@ -18,15 +18,24 @@ float multiply(float x, float y){
 	return x * y;
 }
 
-/*float divide(float x, float y){
-	return divide x / y;
-}*/
+float divide(float x, float y){
+	return x/y;
+}
 
 float domath(float (*mathop) (float, float), float x, float y){
 	return (*mathop) (x, y);
 }
 
 void getOp(char *op){
+	printf("Give math operation (+,-,*,/): ");
+	while(1){
+		scanf("%s", op);
+
+	if(*op == '+' || *op == '-' || *op == '*' || *op == '/')
+		break;
+	else
+		printf("\nIncorrect operation, give +,-,*,/: ");
+	}
 }
 
 void getNum(float *a, float *b){
@@ -42,9 +51,26 @@ int main(void){
 	float a,b;
 	char op;
 	getNum(&a, &b);
-	getOp(&op);
 	printf("%f\n", a);
 	printf("%f\n", b);
+	getOp(&op);
+	if(op == '+'){
+		printf("Sum of %2.2f and %2.2f is %2.2f\n.", a, b, domath(add, a, b));
+	}
+	if(op == '-'){
+		printf("Difference of %2.2f and %2.2f is %2.2f\n.", a, b, domath(subtract, a, b));
+	}
+	if(op == '*'){
+		printf("Product of %2.2f and %2.2f is %2.2f\n.", a, b, domath(multiply, a, b));
+	}
+	if(op == '/'){
+		if(b == 0){
+			printf("Division by zero detected! Aborted.\n");
+		}
+		else
+		printf("Division of %2.2f and %2.2f is %2.2f\n.", a, b, domath(divide, a, b));
+	}
+	//float result = domath(op, a, b);
 
 	return 0;
 ////////float a = domath(add, 10, 2);
